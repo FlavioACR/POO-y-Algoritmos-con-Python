cfd945914778b43559fed8995da0b4762e7e70ee
Flavio Abat Carrola Reta <101416743+FlavioACR@users.noreply.github.com>
2022-05-19 13:45:11 -0500

    Agregando mas codigo al archivo y algo de comentarios

--- 4. Descompisiciones.py
+++ 4. Descompisiciones.py
@@ -1,33 +1,45 @@
-# DESCOMPOSICIÓN 
-#   Partir un problema en problemas mas pequeños
-#   Las clases permiten crear mayores abstracciones en forma de componentes
-#   Cada clase se encarga de una parte delproblema y del programa se vuelve mas facil de mantener.
+'''DESCOMPOSICIÓN 
+* Partir un problema en problemas mas pequeños
+* Las clases permiten crear mayores abstracciones en forma de componentes
+* Cada clase se encarga de una parte delproblema y del programa se vuelve mas facil de mantener.
+'''
+# Ejemplo para comprender la descomposición:
 
-# Ejemplo descomponer un automovil:
-
-from curses import noecho
-
-
+# 1 Creamos una clase llamada automovil.
 class Automovil:
-    
+    # La inicializamos y agregamos atributos de la clase
     def __init__(self, modelo, marca, color):
+        # Atributos propios:
         self.modelo = modelo
         self.marca = marca
         self.color = color
+        # Metodos privados:
         self._estado = 'en_reposo'
         # Despues de definir las clases de motor y sus funciones
-        self._motor = None
-    
+        # Instanciamos una de las clases hijas.
+        self._motor = Motor(cilindros=4)
+
+    # Metodos:
     def acelerar(self, tipo='despacio'):
-        
+        if tipo = 'rapida':
+            # Llamamos a otra clase para regular la inyeccion clase ajena:
+            self._motor.inyecta_gasolina(10)
+        else:
+            # Repetimos el paso if pero con una menor inyección de combustible:
+            self._motor.inyecta_gasolina(3)
+        # Modifica el estado del automovil.
+        self._estado = 'en_movimiento'
 
 class Motor:
-
+    # Parametro por defecto tipo='gasolina', cuando es asi se especifica o no:
     def __init__(self, cilindros, tipo='gasolina'):
         self.cilindros = cilindros
         self.tipo = tipo
+        # Metodos privados:
         self._temperatura = 0
 
+    # Metodos:
+    # Cantidad = Recibe la cantidad de gasolina.
     def inyecta_gasolina(self, cantidad):
          pass
 
---
+++ 5. Abstraccion.py
@@ -0,0 +1,14 @@
+'''Adstracción:
+
+* Enfocarnos en la información relevante, iteractuando con otros objetos.
+* Separar la información central de los detalles secundarios.
+* Podemos utilizar variables y métodos (privados o públicos).
+
+Ejemplo:
+Cuando manejas no te preocupas por el funcionamiento del motor o cuando usas un elevador solo te preocupas en usar el 
+elevador pero no en su funcionamiento de el sistema y poleas.
+
+Para hacer esto se genera una interfaz para interactuar con las clases.
+
+
+'''
