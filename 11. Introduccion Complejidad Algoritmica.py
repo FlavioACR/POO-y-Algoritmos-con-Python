5147a98b55341f8630c3c8f00c30acf3ab5d0456
Flavio Abat Carrola Reta <101416743+FlavioACR@users.noreply.github.com>
2022-05-20 10:05:05 -0500

    " creación de Introducción complejidad algoritmica.py "

---
+++ 11. Introduccion Complejidad Algoritmica.py
@@ -0,0 +1,14 @@
+'''Complejidad Algoritmica:
+
+* ¿ Por qué compararmos la eficiencia de un algoritmo ?
+* Complejidad temporal vs complejidad espacial(memoria)
+* Podemos definirla como T(n)
+
+Implementaciónde T(n)
+Aproximaciones:
+* Cronometrar el tiempo en el que corre un algoritmo
+* Contar los pasos con una medida abstracta de operación
+* Contar los pasos conforme nos aproximamos al infinito (Estandar)
+
+'''
+
