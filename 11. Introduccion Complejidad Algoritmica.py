46f1effe0525e043a4f2b90983f7d82819b3c4fe
FlavioACR <flavioabatcarrolar@outlook.com>
2022-05-20 15:18:25 +0000

    Agregando archivo 11 Intro algo

--- 11. Introduccion Complejidad Algoritmica.py
+++ 11. Introduccion Complejidad Algoritmica.py
@@ -12,3 +12,33 @@ Aproximaciones:
 
 '''
 
+# Para medir el tiempo em python usaremos este modulo:
+import time 
+
+# Generar dos implementaciones factorial:
+
+def factorial(n):
+    respuesta = 1
+
+    while n > 1:
+        respuesta *= n
+        n -= 1
+    return respuesta
+
+# Factorial recursivo
+def factorial_r(n):
+    if n == 1:
+        return 1
+    
+    return n * factorial_r(n - 1)
+
+if __name__=='__main__':
+    n = 200000
+
+    comienzo = time.time()
+    final = time.time()
+    print(final - comienzo)
+
+    comienzo = time.time()
+    final = time.time()
+    print(final - comienzo)
